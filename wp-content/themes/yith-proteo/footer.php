f625236b2cf7d09775d07567887b185f523fc49a
elmahdi <elmahdi.rammach@gmail.com>
2021-07-01 09:45:02 +0100

    add file report

--- wp-content/themes/yith-proteo/footer.php
+++ wp-content/themes/yith-proteo/footer.php
@@ -35,13 +35,13 @@ if ( 'on' !== $hide_footer ) :
 			</div>
 			<?php endif; ?><?php if ( 'yes' === get_theme_mod( 'yith_proteo_footer_sidebar_2_enable', 'yes' ) ) : ?>
 			<div class="footer-sidebar-2">
-				<div class="row"><?php dynamic_sidebar( 'footer-sidebar-2' ); ?></div>
+				<div class="row"><?php  echo '# e-store - The Best in Riklam'?></div>
 			</div>
 			<?php endif; ?></div>
 		<div class="site-info">
 			<div class="container">
 				<?php
-				echo do_shortcode( get_theme_mod( 'yith_proteo_footer_credits_content', __( '<a href="https://proteo.yithemes.com/" target="_blank" rel="noopener">Proteo</a> - A free theme designed with <span class="lnr lnr-heart"></span> by <a href="https://yithemes.com" rel="nofollow">YITH</a>', 'yith-proteo' ) ) );
+				// echo do_shortcode( get_theme_mod( 'yith_proteo_footer_credits_content', __( '<a href="https://proteo.yithemes.com/" target="_blank" rel="noopener">Proteo</a> - A free theme designed with <span class="lnr lnr-heart"></span> by <a href="https://yithemes.com" rel="nofollow">YITH</a>', 'yith-proteo' ) ) );
 				?>
 			</div>
 		</div><!-- .site-info -->
